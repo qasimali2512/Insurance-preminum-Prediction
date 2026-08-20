f9b7b8b715200e3de2a364045b63eded7d3fbaba
Qasim Ali <qasimali251204@gmail.com>
2026-08-20 17:41:20 +0500

    Update Loader.jsx

--- insurance-frontend/src/components/Loader.jsx
+++ insurance-frontend/src/components/Loader.jsx
@@ -1,6 +1,12 @@
+#Loader file
+
 export default function Loader() {
   return (
     <div className="loader-container">
+
+
+
+      #loader
       <div className="loader"></div>
       <p>Analyzing Data...</p>
     </div>
